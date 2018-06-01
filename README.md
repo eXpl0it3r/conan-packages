590fba1a88dbab7b89c7f22ce80238987b3a35c0
Lukas Dürrenberger <lukas.duerrenberger@zuehlke.com>
2018-06-01 11:29:12 +0200

    Initial commit with support for physfs

---
+++ README.md
@@ -0,0 +1,17 @@
+# Conan Packages
+
+This repository is a collection of [Conan](https://conan.io/) packages I maintain.
+If you encounter any issues with the packages themselves, feel free to open an issue.
+If you have issues with the packaged library, please refer to the library's issue tracker.
+
+## List of Conan Packges
+
+* [physfs](https://icculus.org/physfs/) - PhysicsFS is a library to provide abstract access to various archives.
+
+## What is Conan?
+
+[Conan](https://conan.io/) is a portable package manager, intended for C and C++ developers, but it is able to manage builds from source, dependencies, and precompiled binaries for any language.
+
+## License
+
+This software is dual licensed under the Public Domain and the MIT license, choose whichever you prefer. See the LICENSE.md file for more details.
---
+++ physfs/conanfile.py
@@ -0,0 +1,38 @@
+from conans import ConanFile, CMake, tools
+
+
+class PhysfsConan(ConanFile):
+    name = "physfs"
+    version = "3.0.1"
+    license = "zlib"
+    url = "https://github.com/eXpl0it3r/conan-packages"
+    description = "PhysicsFS is a library to provide abstract access to various archives."
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False]}
+    default_options = "shared=False"
+    generators = "cmake"
+
+    def source(self):
+        tools.get("https://hg.icculus.org/icculus/physfs/archive/release-%s.zip" % self.version)
+        # This small hack might be useful to guarantee proper /MT /MD linkage
+        # in MSVC if the packaged project doesn't have variables to set it properly
+        tools.replace_in_file("physfs-release-%s/CMakeLists.txt" % self.version, "project(PhysicsFS)",
+                              """project(PhysicsFS)
+include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
+conan_basic_setup()""")
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure(source_folder="physfs-release-%s" % self.version)
+        cmake.build()
+
+    def package(self):
+        self.copy("physfs.h", dst="include", src="physfs-release-%s/src" % self.version)
+        self.copy("*physfs.lib", dst="lib", keep_path=False)
+        self.copy("*.dll", dst="bin", keep_path=False)
+        self.copy("*.so", dst="lib", keep_path=False)
+        self.copy("*.dylib", dst="lib", keep_path=False)
+        self.copy("*.a", dst="lib", keep_path=False)
+
+    def package_info(self):
+        self.cpp_info.libs = ["physfs"]
